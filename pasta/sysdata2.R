d7a8c31c65db1578337104f0b4c816f3928f45a1
ibarraespinosa <zergioibarra@gmail.com>
2026-04-19 17:51:25 -0400

    ammonia production

--- pasta/sysdata2.R
+++ pasta/sysdata2.R
@@ -680,7 +680,7 @@ sysdata[unit %in% "g/tonne", unit := "g/Mg"]
 
 # NH3
 (sysdata[grepl("NH3", unit), unique(unit)] -> mgg)
-sysdata[unit %in% c("g/ton NH3", "g/t NH3"), unit := "g/t NH3"]
+sysdata[unit %in% c("g/ton NH3", "g/t NH3"), unit := "g/Mg"]
 
 save(sysdata, file = "R/sysdata.rda", compress = "xz")
 rm(list = ls())
